b470e6eabf9151feb78c8086f7987cabb3f732e6
nmarks <nmarkspdx@gmail.com>
2025-01-04 16:35:50 -0800

    preapre for v0.1.1

--- packages/dopetracks/setup.py
+++ packages/dopetracks/setup.py
@@ -2,14 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="dopetracks",
-    version="0.1.0",
+    version="0.1.1",
     author="Nick Marks",
     author_email="nmarkspdx@gmail.com",
     url="https://github.com/namarks/dopeventures",  # GitHub repo URL
-    packages=find_packages(),  # Look for packages in src/
+    packages=find_packages(),  
     # package_dir={"": "dopetracks/dopetracks"},              # Base directory for packages
     python_requires=">=3.11.11",
-
     entry_points={
         "console_scripts": [
             # Add CLI commands if needed, e.g., 'dopetracks=dopetracks_summary.main:main',
